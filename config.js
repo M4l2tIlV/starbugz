54fedf5bae40474e33abe7eaa92fbf430c52f541
M4l2tllV <gentleman_furious@hotmail.com>
2019-10-19 14:21:42 +0700

    Add more files

---
+++ config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  title: 'StarBugZ Blog', // Required
+  author: 'StarBugZer', // Required
+  description: 'Anti Starbugz Starbugz Club',
+  primaryColor: '#3adb34', // Required
+  showHeaderImage: true,
+  showShareButtons: true,
+  postsPerPage: 8, // Required
+  social: {
+    website: 'https://starbugz.club',
+    github: 'https://github.com/M4l2tIlV',
+    facebook: 'https://www.facebook.com/StarbugZ-414710212600916/',
+    email: 'root@starbugz.club',
+  },
+  pathPrefix: '/',
+  siteUrl: 'https://starbugz.club/',
+};
